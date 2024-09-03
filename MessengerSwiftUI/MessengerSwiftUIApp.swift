cc41be17a0712cdef4df810c4ca883d63af66907
Andrei <garnash1313@gmail.com>
2024-09-03 13:56:26 +0300

    Initial Commit

---
+++ MessengerSwiftUI/MessengerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerSwiftUIApp.swift
+//  MessengerSwiftUI
+//
+//  Created by Andrei Harnashevich on 3.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
